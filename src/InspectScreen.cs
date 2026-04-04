2b32b5b4c9cd2390f6c2423deb0ed5acbc14d476
Indi Esneault <indiesneault@gmail.com>
2026-04-04 12:16:05 -0700

    add Apply to Visible Cards button

--- src/InspectScreen.cs
+++ src/InspectScreen.cs
@@ -6,6 +6,7 @@ using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Nodes.Cards;
 using MegaCrit.Sts2.Core.Nodes.GodotExtensions;
 using MegaCrit.Sts2.Core.Nodes.Screens;
+using MegaCrit.Sts2.Core.Nodes.Screens.CardLibrary;
 using MegaCrit.Sts2.Core.Nodes.Screens.Settings;
 
 namespace BalatroEffects;
@@ -16,11 +17,13 @@ public partial class InspectScreen
     private const string paginatorName = "BalatroEffectsPaginator";
     private const string sliderName = "BalatroEffectsSlider";
     private static readonly Lazy<Font> LabelFont = new(() =>
-        GD.Load<Font>("res://themes/kreon_regular_shared.tres")
+        GD.Load<Font>("res://themes/kreon_bold_glyph_space_two.tres")
     );
 
     private static string cardId = "UnknownCard";
     private static int currentEffectIndex;
+
+    private static List<CardModel> visibleCards = [];
 
     public static void AddButtons(Control root)
     {
@@ -32,31 +35,22 @@ public partial class InspectScreen
         slider.Name = sliderName;
         slider.ValueChanged += OnSliderChanged;
 
-        var paginator = (Control)
-            GD.Load<PackedScene>("res://scenes/screens/paginator.tscn").Instantiate();
-        var fxpaginator = new FXPaginator
-        {
-            Name = paginatorName,
-            CustomMinimumSize = new Vector2(0, 64),
-        };
-
-        foreach (Node child in paginator.GetChildren().ToArray())
-        {
-            paginator.RemoveChild(child);
-            fxpaginator.AddChild(child);
-        }
-
         var vbox = new VBoxContainer { Name = vboxName };
         vbox.AddThemeConstantOverride("separation", 8);
 
         root.AddChild(vbox);
 
         vbox.AddChild(CreateLabel("Effect"));
-        vbox.AddChild(fxpaginator);
+        vbox.AddChild(new EffectsPaginator());
+
         vbox.AddChild(CreateDivider());
 
         vbox.AddChild(CreateLabel("Intensity"));
         vbox.AddChild(slider);
+
+        vbox.AddChild(CreateDivider());
+
+        vbox.AddChild(new ApplyToVisibleButton());
 
         vbox.SetAnchorsAndOffsetsPreset(Control.LayoutPreset.BottomRight);
         vbox.Position += 50 * Vector2.Left;
@@ -72,18 +66,24 @@ public partial class InspectScreen
         };
     }
 
-    private static MegaLabel CreateLabel(string text)
+    private static MegaLabel CreateLabel(string text, int fontSize = 28)
     {
         var label = new MegaLabel()
         {
             HorizontalAlignment = HorizontalAlignment.Center,
             VerticalAlignment = VerticalAlignment.Center,
-            MinFontSize = 28,
-            MaxFontSize = 28,
+            MinFontSize = fontSize,
+            MaxFontSize = fontSize,
         };
         label.AddThemeFontOverride(ThemeConstants.Label.Font, LabelFont.Value);
         label.AddThemeColorOverride(ThemeConstants.Label.FontColor, StsColors.cream);
+        label.AddThemeColorOverride(
+            ThemeConstants.Label.FontOutlineColor,
+            StsColors.halfTransparentBlack
+        );
+        label.AddThemeConstantOverride("outline_size", 12);
         label.SetTextAutoSize(text);
+        label.SetAnchorsAndOffsetsPreset(Control.LayoutPreset.FullRect);
         return label;
     }
 
@@ -92,10 +92,98 @@ public partial class InspectScreen
         Config.SetIntensity(currentEffectIndex, (float)value / 100.0f);
     }
 
-    public partial class FXPaginator : NPaginator
+    public partial class ApplyToVisibleButton : NSettingsButton
     {
+        private TextureRect? _image;
+
+        public ApplyToVisibleButton()
+        {
+            CustomMinimumSize = new Vector2(0, 64);
+        }
+
         public override void _Ready()
         {
+            var img = new TextureRect()
+            {
+                Texture = GD.Load<CompressedTexture2D>(
+                    "res://images/ui/reward_screen/reward_skip_button.png"
+                ),
+                ExpandMode = TextureRect.ExpandModeEnum.IgnoreSize,
+                StretchMode = TextureRect.StretchModeEnum.Scale,
+                Size = new Vector2(320, 64),
+            };
+            img.SetAnchorsAndOffsetsPreset(LayoutPreset.FullRect);
+            _image = img;
+            AddChild(img);
+            AddChild(CreateLabel("Apply to Visible Cards", 24));
+            AddChild(GD.Load<PackedScene>("res://scenes/ui/selection_reticle.tscn").Instantiate());
+            ConnectSignals();
+            VisibilityChanged += DisableWhenNotInCompendium;
+            DisableWhenNotInCompendium();
+            CallDeferred(nameof(UpdatePivot));
+        }
+
+        private void UpdatePivot()
+        {
+            PivotOffset = Size * 0.5f;
+        }
+
+        private void DisableWhenNotInCompendium()
+        {
+            if (visibleCards.Count > 0)
+            {
+                Enable();
+            }
+            else
+            {
+                Disable();
+            }
+        }
+
+        protected override void OnPress()
+        {
+            foreach (CardModel c in visibleCards)
+            {
+                Config.SetEffect(c.Id.ToString(), Config.GetEffect(cardId));
+            }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            _image!.Modulate = Colors.White;
+            Modulate = Colors.White;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            _image!.Modulate = Colors.Gray;
+            Modulate = Colors.Gray;
+        }
+    }
+
+    public partial class EffectsPaginator : NPaginator
+    {
+        public EffectsPaginator()
+        {
+            Name = paginatorName;
+            CustomMinimumSize = new Vector2(0, 64);
+        }
+
+        public override void _Ready()
+        {
+            var paginator = (Control)
+                GD.Load<PackedScene>("res://scenes/screens/paginator.tscn").Instantiate();
+
+            foreach (Node child in paginator.GetChildren().ToArray())
+            {
+                paginator.RemoveChild(child);
+                AddChild(child);
+            }
+
+            paginator.QueueFree();
+
             _label = (MegaLabel)FindChild("Label");
             ref MegaLabel vfxField = ref AccessTools.FieldRefAccess<NPaginator, MegaLabel>(
                 "_vfxLabel"
@@ -160,11 +248,31 @@ public partial class InspectScreen
             ShaderController.ApplyShader(cardNode);
             string cardId = ____cards[index].Id.ToString();
 
-            var paginator = __instance.GetNodeOrNull<FXPaginator>(vboxName + "/" + paginatorName);
+            var paginator = __instance.GetNodeOrNull<EffectsPaginator>(
+                vboxName + "/" + paginatorName
+            );
             paginator?.UpdateTargetCard(cardId);
 
             var slider = __instance.GetNodeOrNull<NSlider>(vboxName + "/" + sliderName);
             UpdateSliderVisual(slider);
+        }
+    }
+
+    [HarmonyPatch(typeof(NCardLibraryGrid), "DisplayCards")]
+    static class ApplyToVisibleCardsPatch
+    {
+        public static void Postfix(List<CardModel> cards)
+        {
+            visibleCards = cards;
+        }
+    }
+
+    [HarmonyPatch(typeof(NCardLibrary), nameof(NCardLibrary.OnSubmenuClosed))]
+    static class ClearVisiblecardsPatch
+    {
+        public static void Postfix()
+        {
+            visibleCards = [];
         }
     }
 }
